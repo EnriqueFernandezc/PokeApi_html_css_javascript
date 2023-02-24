305efef11a858e3b2f84697b1ec0e4e3ffa8be24
Enrique Fernandez <fernandezcampoenrique@gmail.com>
2023-02-24 20:46:53 +0100

    aplicar estilos css

--- main.js
+++ main.js
@@ -9,3 +9,8 @@ fetch('https://pokeapi.co/api/v2/pokemon/1/')
         let size=data.height;
         let stats=data.stats;
     });
+
+    // el boton home debe blanquear la pantalla
+    // next es el siguiente pokemon
+    // back es el pokemon anterior
+    // el otro boton es para buscar el pokemon y mostrarlo en pantalla
