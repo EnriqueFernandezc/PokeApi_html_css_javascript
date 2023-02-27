a03ecba86187107e18d6fe7ae624f3dcfc65dfdd
Enrique Fernandez <fernandezcampoenrique@gmail.com>
2023-02-27 13:26:36 +0100

    usar fetch para mostrar un pokemon solicitado por el usuario

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-fetch('https://pokeapi.co/api/v2/pokemon/1/')
-    .then(Response=>Response.json())
-    .then(data=>{
+// el boton home debe blanquear la pantalla
+// next es el siguiente pokemon
+// back es el pokemon anterior
+// el otro boton es para buscar el pokemon y mostrarlo en pantalla
 
-        console.log(data);
-        let id=data.id;
-        let name=data.name;
-        let type=data.types;
-        let size=data.height;
-        let stats=data.stats;
-    });
+let input=document.querySelector('#input');
+let search=document.querySelector('#search');
+let screen=document.querySelector('#screen');
+let home=document.querySelector('#button-home')
 
-    // el boton home debe blanquear la pantalla
-    // next es el siguiente pokemon
-    // back es el pokemon anterior
-    // el otro boton es para buscar el pokemon y mostrarlo en pantalla
+
+search.addEventListener('click', ()=>{
+    let pokemon=input.value;
+
+    let ruta=`https://pokeapi.co/api/v2/pokemon/${pokemon}/`
+
+    fetch(ruta)
+        .then(Response=>Response.json())
+        .then(data=>{
+
+            console.log(data);
+            let id=data.id;
+            let name=data.name;
+            let type=data.types;
+            let size=data.height;
+            let stats=data.stats;
+
+            screen.innerHTML=`<img src="${data.sprites.front_default}" alt="${data.name}" width="80%">`
+        
+        });
+});
